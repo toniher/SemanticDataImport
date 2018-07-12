5d667f8b4708bf1c7929a307e10d1ffeba2a3ca4
yassin98 <yassinassbaghi@gmail.com>
2018-07-12 10:12:07 +0200

    recovering single

--- includes/SDImport.special.php
+++ includes/SDImport.special.php
@@ -72,8 +72,12 @@ class SpecialSDImport extends SpecialPage {
 				'type' => 'select',
 				'label' => 'Namespace',
 				'options' => $list_namespaces
-			)
-			
+			),
+			'single' => [
+				'class' => 'HTMLCheckField',
+				'label' => 'Single row mode',
+				'default' => true,
+			]		
 
 		);
 
